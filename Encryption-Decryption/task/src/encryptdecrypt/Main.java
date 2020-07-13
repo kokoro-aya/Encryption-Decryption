f53d55f6dd4fc037d8d24154d1ab50e1247fa145
alicenychto <irol2273@icloud.com>
2020-07-13 22:46:46 +0200

    stage 3 cleared

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -4,27 +4,49 @@ import java.util.Scanner;
 
 public class Main {
 
-    static String convert(String src, int key) {
+//    static String convert(String src, int key) {
+//        String dst = "";
+//        for (char c: src.toCharArray()) {
+//            if (Character.isAlphabetic(c)) {
+//                if (c + key <= 'z') {
+//                    dst += (char) (c + key);
+//                } else {
+//                    dst += (char) ('a' + c + key - 'z' - 1);
+//                }
+//            }
+//            else {
+//                dst += c;
+//            }
+//        }
+//        return dst;
+//    }
+
+    static String encode(String src, int key) {
         String dst = "";
         for (char c: src.toCharArray()) {
-            if (Character.isAlphabetic(c)) {
-                if (c + key <= 'z') {
-                    dst += (char) (c + key);
-                } else {
-                    dst += (char) ('a' + c + key - 'z' - 1);
-                }
-            }
-            else {
-                dst += c;
-            }
+            dst += (char) (c + key);
+        }
+        return dst;
+    }
+
+    static String decode(String src, int key) {
+        String dst = "";
+        for (char c: src.toCharArray()) {
+            dst += (char) (c - key);
         }
         return dst;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String method = sc.nextLine();
         String src = sc.nextLine();
         int key = sc.nextInt();
-        System.out.println(convert(src, key));
+        if (method.equals("enc")) {
+            System.out.println(encode(src, key));
+        }
+        if (method.equals("dec")) {
+            System.out.println(decode(src, key));
+        }
     }
 }
